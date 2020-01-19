1bb44a2bc4d13818aeb3be87d0736ceeef4e2580
zhangwengao <zhangwengao@yy.com>
2020-01-19 18:36:29 +0800

    no message

---
+++ app/src/main/java/com/optimize/performance/aop/PerformanceAop.java
@@ -0,0 +1,81 @@
+package com.optimize.performance.aop;
+
+import android.util.Log;
+
+import com.optimize.performance.utils.LogUtils;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
+@Aspect
+public class PerformanceAop {
+    @Around("call(* com.optimize.performance.PerformanceApp.initFresco(..))")
+    public void getTime(ProceedingJoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        String name = signature.toShortString();
+        long time = System.currentTimeMillis();
+        try {
+            joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        LogUtils.i(name + "PerformanceAop getTime cost " + (System.currentTimeMillis() - time));
+    }
+
+    // @Pointcut("execution (* com.optimize.performance.PerformanceApp.initJPush(..))")
+    // public void pointcut() {
+    //     Log.e("TAG", "PerformanceAop TestAnnoAspect pointcut:");
+    // }
+    //
+    // @Before("pointcut()")
+    // public void before(JoinPoint point) {
+    //     Log.e("TAG", "PerformanceAop TestAnnoAspect before:");
+    // }
+    //
+    // @Around("pointcut()")
+    // public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+    //     Log.e("TAG", "PerformanceAop TestAnnoAspect around:");
+    //     try {
+    //         joinPoint.proceed();
+    //     } catch (Throwable throwable) {
+    //         throwable.printStackTrace();
+    //     }
+    //
+    // }
+    //
+    //
+    // @After("pointcut()")
+    // public void after(JoinPoint point) {
+    //     Log.e("TAG", "TestAnnoAspect after:");
+    // }
+    //
+    // @AfterReturning("pointcut()")
+    // public void afterReturning(JoinPoint point, Object returnValue) {
+    //     Log.e("TAG", "TestAnnoAspect afterReturning:");
+    // }
+
+    @Around("execution(* android.app.Activity.setContentView(..))")
+    public void getSetContentViewTime(ProceedingJoinPoint joinPoint) {
+        Signature signature = joinPoint.getSignature();
+        String name = signature.toShortString();
+        long time = System.currentTimeMillis();
+        try {
+            joinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+        LogUtils.i(name + " cost " + (System.currentTimeMillis() - time));
+    }
+
+
+
+
+
+}
